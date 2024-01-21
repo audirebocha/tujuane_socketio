75fe097ba28485e6f32156654d9b6558b92dc9f2
Peter Kimutai <135107576+audirebocha@users.noreply.github.com>
2024-01-21 16:14:26 +0300

    Update Db_con.js

--- Db_con.js
+++ Db_con.js
@@ -1,14 +1,1 @@
-import mongoose from "mongoose";
 
-
-async function db_connect(){
-    var uri="mongodb+srv://root:123@cluster0.u5atqck.mongodb.net/tujuane?retryWrites=true&w=majority"
-    try{
-        await mongoose.connect(uri);
-        console.log('Connected to Mongo DB');
-    }catch(error){
-        console.error(error)
-    }
-}
-
-export default db_connect
